e30a5c48257a2c9df2dec8f556bb0e8b9edbca79
darealest <zozobozza123@gmail.com>
2025-10-31 14:02:35 +0700

    chuẩn bị deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,8 +3,10 @@ import tasksRoute from './routes/tasksRouters.js'
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -16,8 +18,15 @@ app.use(express.json());
 
 app.use(cors({origin:"http://localhost:5173"}));
 
-app.use("/api/tasks", tasksRoute)
+app.use("/api/tasks", tasksRoute);
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+}
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log(`Server bắt đầu trên cổng ${PORT}`);
